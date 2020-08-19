72da7f7601f45e0dfc176a2c337ed3a1b43281bc
Crisscrisis <mrcrisscrisis@gmail.com>
2020-08-19 10:22:01 +0800

    add modification of weekly contest 0816(leetcode 1551, leetcode 1552, leetcode 1553)

--- weekly_contest/200816/SOLUTION.cpp
+++ weekly_contest/200816/SOLUTION.cpp
@@ -1,6 +1,6 @@
 /* weekly contest
  * 2020/08/16
- *
+ * leetcode 1550, 1551, 1552
  *
  */
 #include <vector>
@@ -12,6 +12,7 @@
 using namespace std;
 class Solution {
 public:
+    // leetcode 1550
     bool threeConsecutiveOdds(vector<int>& arr) {
         if (arr.size() < 3) {
             return false;
@@ -30,6 +31,7 @@ public:
         return false;
     }
 
+    // leetcode 1551
     int minOperations(int n) {
         if (n == 1) {
             return 0;
@@ -42,44 +44,87 @@ public:
         return operation;
     }
 
+    // leetcode 1552
+    bool isMatched(vector<int>& position, int m, int dis) {
+        int cnt = 0;
+        int pre = position[0];
+        int len = position.size();
+        for(int i = 1; i < len; i++) {
+            if (position[i] - pre >= dis && position[len - 1] - position[i] >= dis) { // current position to left, right borad >= distance
+                pre = position[i];
+                cnt++;
+            }
+            if (cnt == m - 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     int maxDistance(vector<int>& position, int m) {
         sort(position.begin(), position.end());
         int len = position.size();
-        vector<int> distance(len);
-        distance[0] = 0;
+        int minDis = INT_MAX;
         for(int i = 1; i < len; i++) {
-            distance[i] = position[i] - position[i-1];
-        }
-        sort(distance.begin(), distance.end());
-        int maxDis = (position[len - 1] - position[0]) / (m - 1);
-        int pre, resNum;
-        int j = len - 1;
-        for (int dis = maxDis; dis > 0; dis = distance[j--]) {
-            pre = position[0];
-            resNum = m - 2;
-            for (int i = 1; i < len - 1; i ++) {
-                if (position[i] - pre >= dis) {
-                    pre = position[i];
-                    resNum--;
-                }
-                if (resNum == 0 && position[len - 1] - pre >= dis) {
-                    return min(dis, position[len - 1] - pre);
-                }
+            if (minDis > position[i] - position[i-1]) {
+                minDis = position[i] - position[i-1];
             }
         }
-        return 1;
+        int maxDis = (position[len - 1] - position[0]) / (m - 1);
+        int l,r;
+        l = minDis;
+        r = maxDis;
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if(isMatched(position, m, mid)) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return l-1;
+    }
+
+    // leetcode 1552
+    unordered_map<int, int> ans;
+    int dfs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (ans.count(n)) { // find ans[n]
+            return ans[n];
+        }
+        int tmp = min(dfs(n/3) + n % 3 + 1, dfs(n/2) + n % 2 + 1);
+        ans[n] = tmp;
+        return tmp;
+    }
+    int minDays(int n) {
+        return dfs(n);
     }
 };
 
 
 /**************************   run solution   **************************/
+#if 0 //leetcode 1552
 int _solution_run(vector<int>& position, int m)
 {
     Solution weekly_contest;
     int ans = weekly_contest.maxDistance(position, m);
     return ans;
 }
-
+#else
+// leetcode 1553
+int _solution_run(int n)
+{
+    Solution weekly_contest;
+    int ans = weekly_contest.minDays(n);
+    return ans;
+}
+#endif
 #ifdef USE_SOLUTION_CUSTOM
 string _solution_custom(TestCases &tc)
 {
